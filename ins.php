688fa37d911397e6435bd3452712f9cd59f3b001
Ryoneme <75056575+Ryonem2@users.noreply.github.com>
2021-09-02 07:20:43 +0700

    done

--- ins.php
+++ ins.php
@@ -1,17 +1,34 @@
 <?php
-//save data to database
 
 require('dbconnect.php');
 
-$postData = file_get_contents('php://input');
-$jsondata = json_decode($postData);
-// echo "postData =".json_encode($jsondata->name,JSON_UNESCAPED_UNICODE);
-    $data =  json_encode($jsondata->data);
+//?POST Multifile
+
+// foreach ($_FILES["avatar"]["error"] as $key => $error) {
+//     if ($error == UPLOAD_ERR_OK) {
+//         $tmp_name = $_FILES["avatar"]["tmp_name"][$key];
+
+//         $type = pathinfo($tmp_name, PATHINFO_EXTENSION);
+//         $data = file_get_contents($tmp_name);
+//         $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        
+//     }
+// }
+// echo $base64;
 
 
-// mysql_query("SET NAMES UTF8");
-$sql = "INSERT INTO testt (NAME,SERNAME,SIG) VALUES ('aaa','aaa',$data)";
+//?Post Single File
+$tmp_name = $_FILES["avatar"]["tmp_name"][0]; //Create temp path
 
+$type = pathinfo($tmp_name, PATHINFO_EXTENSION);
+$data = file_get_contents($tmp_name);
+// echo $data;
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+
+//save data to database
+mysql_query("SET NAMES UTF8");
+$sql = "INSERT INTO testt (NAME,SERNAME,SIG) VALUES ('aaa','aaa',$base64)";
 
 $result = mysqli_query($connect,$sql); //save data
 
