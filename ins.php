41b7793a0f0f8e6dc2baa6b6a7793d16f2e6a129
Ryoneme <75056575+Ryonem2@users.noreply.github.com>
2021-09-02 20:36:00 +0700

    fix bug cant find type

--- ins.php
+++ ins.php
@@ -7,23 +7,31 @@ require('dbconnect.php');
 // foreach ($_FILES["avatar"]["error"] as $key => $error) {
 //     if ($error == UPLOAD_ERR_OK) {
 //         $tmp_name = $_FILES["avatar"]["tmp_name"][$key];
-
+        
 //         $type = pathinfo($tmp_name, PATHINFO_EXTENSION);
 //         $data = file_get_contents($tmp_name);
 //         $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
-        
+
 //     }
 // }
+// echo $base64;
+// echo $tmp_name;
+// echo $type;
 // echo $base64;
 
 
 //?Post Single File
-$tmp_name = $_FILES["avatar"]["tmp_name"][0]; //Create temp path
+$tmp_name = $_FILES["avatar"]["tmp_name"][0]; 
+// Create temp path
+// foreach($_FILES["avatar"]["error"] as $key => $error) {
+//     echo $tmp_name = $_FILES["avatar"][$key][0];
+// }
 
 $type = pathinfo($tmp_name, PATHINFO_EXTENSION);
 $data = file_get_contents($tmp_name);
-// echo $data;
+
 $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+echo $base64;
 
 
 //save data to database
